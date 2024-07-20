af8ecc94fbb7cc57183b30c68d90ab906101e760
debasky01 <128429559+debasky01@users.noreply.github.com>
2024-07-19 19:26:37 -0600

    Update Calculator.java

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a + 2 + b;
+        return a + b;
     }
 
     public int subtract(int a, int b) {
